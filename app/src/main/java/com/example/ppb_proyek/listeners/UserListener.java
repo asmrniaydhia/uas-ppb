d5f06168830964f4c8b51fadff76faf96c58b386
asmrniaydhia <asmarani1012@gmail.com>
2025-06-17 20:19:09 +0800

    git commit -m "uas ppb"

---
+++ app/src/main/java/com/example/ppb_proyek/listeners/UserListener.java
@@ -0,0 +1,7 @@
+package com.example.ppb_proyek.listeners;
+
+import com.example.ppb_proyek.models.User;
+
+public interface UserListener {
+    void onUserClicked(User user);
+}
---
+++ ppb_proyek/app/src/main/java/com/example/ppb_proyek/models/ChatMessage.java
@@ -0,0 +1,5 @@
+package com.example.ppb_proyek.models;
+
+public class ChatMessage {
+    public String senderId, receiverId, message, dateTime;
+}
